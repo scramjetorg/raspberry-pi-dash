dfeb5fa7b4a31752a9849153c602d3e96ed8bb21
Martyna Koźbiał <martyna.kozbial@scramjet.org>
2023-02-05 18:44:03 +0100

    feat: create the basis for a new application

---
+++ raspberry-pi/src/components/Chart5.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Line } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+  } from 'chart.js';
+
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
+const Chart5 = () => {
+    const options = {
+        responsive: true,
+        plugins: {
+            title: {
+                display: false,
+            },
+        },
+    };
+    const labels = ['1', '2', '3', '4', '5', '6', '7'];
+    const data = {
+         labels,
+         datasets: [
+           {
+             label: 'Placeholder',
+             data: [23, 23, 13, -4, 14, 23, 34],
+             borderColor: 'rgb(37,119,191)',
+             backgroundColor: 'rgba(37, 119, 191, 0.726)',
+           }
+        ],
+    };
+    return (
+        <div className='chartContainer'>
+            <Line data={data} options={options}/>
+        </div>
+    )
+}
+
+export default Chart5
