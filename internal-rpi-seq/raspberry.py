3ae1fca750658f6e22836371a7bb697d4576cebd
mcdominik <dominik8881@wp.pl>
2023-02-14 16:50:32 +0100

    change mock to real

--- internal-rpi-seq/raspberry.py
+++ internal-rpi-seq/raspberry.py
@@ -1,18 +1,18 @@
 import asyncio
-# from gpiozero import CPUTemperature, DiskUsage, LoadAverage, PingServer
+from gpiozero import CPUTemperature, DiskUsage, LoadAverage, PingServer
 
 
 async def run(context, input, interval=1):
     while True:
-        cpu_temp = 37.234
-        disk_usage = 7.456
-        load_avg = 11.4645
-        # cpu = CPUTemperature()
-        # cpu_temp = cpu.temperature
-        # disk = DiskUsage()
-        # disk_usage = disk.usage
-        # load = LoadAverage()
-        # load_avg = load.load_average()
+        # cpu_temp = 37.234
+        # disk_usage = 7.456
+        # load_avg = 11.4645
+        cpu = CPUTemperature()
+        cpu_temp = cpu.temperature
+        disk = DiskUsage()
+        disk_usage = disk.usage
+        load = LoadAverage()
+        load_avg = load.load_average()
         # google = PingServer('google.com')
         # print(google)
         # print(f'Current disk usage: {disk_usage:.2f}%')
