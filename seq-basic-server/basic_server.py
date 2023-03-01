e0b208792c4f3333a6fa58f315cf5b1577544cd4
Budleigh Salterton <cz@scramjet.org>
2023-03-01 10:39:43 +0000

    Improve static sample

--- seq-basic-server/basic_server.py
+++ seq-basic-server/basic_server.py
@@ -4,7 +4,7 @@ from functools import partial
 
 
 async def run(context, input, port=8002):
-    Handler = partial(http.server.SimpleHTTPRequestHandler, directory='../')
+    Handler = partial(http.server.SimpleHTTPRequestHandler, directory='static/')
     with socketserver.TCPServer(("", port), Handler) as httpd:
         print(f"serving at port: {port}")
         httpd.serve_forever()
