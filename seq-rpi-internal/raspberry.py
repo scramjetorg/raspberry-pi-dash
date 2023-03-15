5309a6948f6bfa6f10f2928a4c62540f2baefa9c
mcdominik <dominik8881@wp.pl>
2023-03-15 13:32:55 +0100

    add auto mock

--- seq-rpi-internal/raspberry.py
+++ seq-rpi-internal/raspberry.py
@@ -1,6 +1,13 @@
 import asyncio
 from scramjet.streams import Stream
-from gpiozero import CPUTemperature, DiskUsage, LoadAverage, PingServer
+from random import randint
+
+mock = False
+try:
+    from gpiozero import CPUTemperature, DiskUsage, LoadAverage, PingServer
+except ImportError:
+    mock = True
+    print('gpiozero module not found, the script proceed with mocked values')
 
 
 provides = {
@@ -8,17 +15,23 @@ provides = {
    'contentType': 'text/plain'
 }
 
-async def set_internals(stream, interval=3):
+async def set_internals(stream, interval=3, mock=mock):
     while True:
-        cpu_temp = round(CPUTemperature().temperature, 2)
-        disk_usage = round(DiskUsage().usage, 2)
-        load_avg = round(LoadAverage().load_average, 2)
-        stream.write([cpu_temp, disk_usage, load_avg])
+        if mock:
+            cpu_temp = randint(-20, 80)
+            disk_usage = randint(0, 99)
+            load_avg = randint(0, 99)
+        else:   
+            cpu_temp = round(CPUTemperature().temperature, 2)
+            disk_usage = round(DiskUsage().usage, 2)
+            load_avg = round(LoadAverage().load_average, 2)
+        stream.write(['ok', cpu_temp, disk_usage, load_avg])
         await asyncio.sleep(interval)
 
 async def run(context, input):
     stream = Stream()
     asyncio.gather(set_internals(stream), return_exceptions=True)
     return stream.map(lambda x : x + "\n")
+
 
 
