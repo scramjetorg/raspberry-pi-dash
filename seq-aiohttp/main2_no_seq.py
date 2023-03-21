c2160de2bee8901cfd1866cc706269eaf0aee392
mcdominik <dominik8881@wp.pl>
2023-03-21 12:57:36 +0100

    new seq scripts

--- seq-aiohttp/main2_no_seq.py
+++ seq-aiohttp/main2_no_seq.py
@@ -2,13 +2,11 @@ from aiohttp import web
 import asyncio
 import functools
 from random import randint
-from pyee.base import EventEmitter
 from pyee.asyncio import AsyncIOEventEmitter
-import nest_asyncio
+
 
 # ee = EventEmitter()
 ee = AsyncIOEventEmitter()
-connected = set()
 
 requires = {
    'requires': 'pi',
@@ -26,13 +24,11 @@ async def push_to_socket(socket, data):
 async def root(request):
     return web.Response(text="working..")
 
-async def websocket_handler(request, input):
+async def websocket_handler(request):
     ws = web.WebSocketResponse()
-    connected.add(ws)
     await ws.prepare(request)
-    # await get_from_topic(ws=ws, input=input)
-
     print('connected')
+    await mock_from_topic(ws=ws)
     async for msg in ws:
         # ws.disconnect() # any messages here should kill the connection
         print("websocket connection started sending data we dont accept")  
@@ -41,23 +37,15 @@ async def websocket_handler(request, input):
 
 async def mock_from_topic(ws):
     while True:
-        ee.emit('event', randint(0,20), ws)
-        await asyncio.sleep(1)
+        ee.emit('event', ws, randint(0,20))
+        await asyncio.sleep(randint(0,4))
 
-async def get_from_topic(input, ws):
-    print(ws)
-    print(input)
-    # return input.map(lambda s: f'consumer got: {s}').each(print)
-    return input.each(lambda s: ee.emit("event", s, ws))
-
-async def run(context, input):
-    nest_asyncio.apply()
-    app = web.Application()
-    bound_handler = functools.partial(websocket_handler, input=input)
-    app.add_routes([web.get('/ws', bound_handler)])
-    app.add_routes([web.static('/files', './', show_index=True)])
-    asyncio.gather(get_from_topic(input, ws=bound_handler), web.run_app(app))
-
+app = web.Application()
+app.add_routes([web.get('/', root)])
+bound_handler = functools.partial(websocket_handler)
+app.add_routes([web.get('/ws', bound_handler)])
+app.add_routes([web.static('/files','./', show_index=True)])
+web.run_app(app)
 
 
 
