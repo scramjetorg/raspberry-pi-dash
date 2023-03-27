a11934cb7fe09901914b03f54a7fa923f6a4934b
Budleigh Salterton <cz@scramjet.org>
2023-03-27 12:27:20 +0000

    Added copying script to the rpi

--- raspberry-pi/seq-basic-server/basic_server.py
+++ raspberry-pi/seq-basic-server/basic_server.py
@@ -2,7 +2,6 @@ import http.server
 import socketserver
 from functools import partial
 
-
 async def run(context, input, port=8002):
     Handler = partial(http.server.SimpleHTTPRequestHandler, directory='static/')
     with socketserver.TCPServer(("", port), Handler) as httpd:
