dfd78b4eff2fae5ce6209d30ec280a1f0c745861
mcdominik <dominik8881@wp.pl>
2023-03-28 17:41:08 +0200

    update

--- seq-aiohttp/main.py
+++ seq-aiohttp/main.py
@@ -2,23 +2,25 @@ from aiohttp import web
 import asyncio
 import functools
 from random import randint
-from pyee.base import EventEmitter
 from pyee.asyncio import AsyncIOEventEmitter
 import nest_asyncio
+import sys
 
-# ee = EventEmitter()
 ee = AsyncIOEventEmitter()
 connected = set()
+
+print(f'PATH SEQ: {sys.path}')
+
 
 requires = {
    'requires': 'pi',
    'contentType': 'text/plain'
 }
 
-@ee.on('event')
-async def event_handler(ws, data):
-    print(f'TRIGGERED, {data}, {ws}')
-    await ws.send_str(f'ok, data {data}')
+# @ee.on('event')
+# async def event_handler(ws, data):
+#     print(f'TRIGGERED, {data}, {ws}')
+#     await ws.send_str(f'ok, data {data}')
 
 async def push_to_socket(socket, data):
     await socket.send_str(f'pushed: {data}')
@@ -26,11 +28,12 @@ async def push_to_socket(socket, data):
 async def root(request):
     return web.Response(text="working..")
 
-async def websocket_handler(request, input):
+async def websocket_handler(request):
     ws = web.WebSocketResponse()
     connected.add(ws)
     await ws.prepare(request)
     # await get_from_topic(ws=ws, input=input)
+    ee.on('event', lambda s: ws.send_str(f'ok, data {s}'))
 
     print('connected')
     async for msg in ws:
@@ -44,20 +47,40 @@ async def mock_from_topic(ws):
         ee.emit('event', randint(0,20), ws)
         await asyncio.sleep(1)
 
-async def get_from_topic(input, ws):
-    print(ws)
-    print(input)
+async def get_from_topic(input):
+    print('enter func')
+    # print(input)
+    async for x in input:
+        ee.emit("event", x)
+        print(x)
+    print('exit func')
+    
+    # # return input.each(print)
+    # # return input.each(lambda s: ee.emit("event", s))
     # return input.map(lambda s: f'consumer got: {s}').each(print)
-    return input.each(lambda s: ee.emit("event", s, ws))
 
 async def run(context, input):
     nest_asyncio.apply()
     app = web.Application()
-    bound_handler = functools.partial(websocket_handler, input=input)
+    bound_handler = functools.partial(websocket_handler)
+    print('hi')
     app.add_routes([web.get('/ws', bound_handler)])
     app.add_routes([web.static('/files', './', show_index=True)])
-    asyncio.gather(get_from_topic(input, ws=bound_handler), web.run_app(app))
 
+
+
+    loop = asyncio.get_event_loop()
+    print(loop)
+    loop.set_debug(True)
+    loop.create_task(web.run_app(app))
+    loop.create_task(get_from_topic(input=input))
+    loop.run_forever()
+
+    # await get_from_topic(input=input)
+    # loop = asyncio.get_running_loop()
+    # print('before topic register')
+    # await loop.run_in_executor(web.run_app(app))
+    # print('after')
 
 
 
