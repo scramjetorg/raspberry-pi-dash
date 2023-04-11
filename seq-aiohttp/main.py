2c3b7dec3dde7a406795753d68954eaa8a2f5e1d
mcdominik <dominik8881@wp.pl>
2023-04-11 09:41:50 +0200

    seq update

--- seq-aiohttp/main.py
+++ seq-aiohttp/main.py
@@ -1,86 +1,35 @@
-from aiohttp import web
-import asyncio
-import functools
-from random import randint
-from pyee.asyncio import AsyncIOEventEmitter
+import aiohttp
 import nest_asyncio
-import sys
-
-ee = AsyncIOEventEmitter()
-connected = set()
-
-print(f'PATH SEQ: {sys.path}')
+from aiohttp import web
+from client.host_client import HostClient
 
 
-requires = {
-   'requires': 'pi',
-   'contentType': 'text/plain'
-}
+api_base ='http://127.0.0.1:8000' 
+host = HostClient(f'{api_base}/api/v1/')
+topic_gen = host.get_named_data('pi')
 
-# @ee.on('event')
-# async def event_handler(ws, data):
-#     print(f'TRIGGERED, {data}, {ws}')
-#     await ws.send_str(f'ok, data {data}')
-
-async def push_to_socket(socket, data):
-    await socket.send_str(f'pushed: {data}')
-
-async def root(request):
-    return web.Response(text="working..")
-
-async def websocket_handler(request):
+async def handler(request):
     ws = web.WebSocketResponse()
-    connected.add(ws)
     await ws.prepare(request)
-    # await get_from_topic(ws=ws, input=input)
-    ee.on('event', lambda s: ws.send_str(f'ok, data {s}'))
 
-    print('connected')
+    async for chunk in await topic_gen:
+        await ws.send_str(str(chunk))
     async for msg in ws:
-        # ws.disconnect() # any messages here should kill the connection
-        print("websocket connection started sending data we dont accept")  
+        if msg.type == aiohttp.WSMsgType.TEXT:
+            if msg.data == 'close':
+                await ws.close()
+            else:
+                await ws.send_str(f'{msg.data} /answer')
+        elif msg.type == aiohttp.WSMsgType.ERROR:
+            print(f'ws connection closed with exception {ws.exception()}')
     print('websocket connection closed')
     return ws
-
-async def mock_from_topic(ws):
-    while True:
-        ee.emit('event', randint(0,20), ws)
-        await asyncio.sleep(1)
-
-async def get_from_topic(input):
-    print('enter func')
-    # print(input)
-    async for x in input:
-        ee.emit("event", x)
-        print(x)
-    print('exit func')
-    
-    # # return input.each(print)
-    # # return input.each(lambda s: ee.emit("event", s))
-    # return input.map(lambda s: f'consumer got: {s}').each(print)
 
 async def run(context, input):
     nest_asyncio.apply()
     app = web.Application()
-    bound_handler = functools.partial(websocket_handler)
-    print('hi')
-    app.add_routes([web.get('/ws', bound_handler)])
     app.add_routes([web.static('/files', './', show_index=True)])
-
-
-
-    loop = asyncio.get_event_loop()
-    print(loop)
-    loop.set_debug(True)
-    loop.create_task(web.run_app(app))
-    loop.create_task(get_from_topic(input=input))
-    loop.run_forever()
-
-    # await get_from_topic(input=input)
-    # loop = asyncio.get_running_loop()
-    # print('before topic register')
-    # await loop.run_in_executor(web.run_app(app))
-    # print('after')
-
+    app.add_routes([web.get('/ws', handler)])
+    await web.run_app(app, port=8020)
 
 
