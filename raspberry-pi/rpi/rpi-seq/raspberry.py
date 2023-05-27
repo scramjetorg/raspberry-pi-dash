4d6f385ca9797afbc6d393f06746f3aed5b0ec6e
gzukowski <gracek_608@wp.pl>
2023-05-27 16:26:34 +0000

    Fixed server's package.json

--- raspberry-pi/rpi/rpi-seq/raspberry.py
+++ raspberry-pi/rpi/rpi-seq/raspberry.py
@@ -15,9 +15,9 @@ params = {"cpu_temp":0,'disk_usage':0,'load_avg':0}
 
 async def run(context, input):
     while True:
-        params['cpu_temp']=5
-        params['disk_usage']  = 5
-        params['load_avg']  = 5
+        params['cpu_temp']=round(CPUTemperature().temperature, 2)
+        params['disk_usage']  = round(DiskUsage().usage, 2)
+        params['load_avg']  = round(LoadAverage().load_average, 2)
         yield json.dumps(params)
         await asyncio.sleep(3) 
 
